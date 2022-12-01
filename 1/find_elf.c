f2d0b2c63aa393ba76deaa26bf8d95976bab2b4a
bebna <mail@bebna.de>
2022-12-01 21:30:38 +0100

    2022-12-01T21:30:38+01:00

--- 1/find_elf.c
+++ 1/find_elf.c
@@ -1,55 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <limits.h>
+#include <string.h>
 
-/* cat filename | awk '{ print length }' | sort -n | tail -1 */
+/* How I chose the LINE_LEN:
+ * $ cat input.txt | awk '{ print length }' | sort -n | tail -1
+ * 5
+ */
 #define LINE_LEN 8 
+
+void swap(unsigned long *a, unsigned long *b) {
+	unsigned long tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 void process_elf(unsigned long *top, unsigned long current_elf) {
 	for(int i=0; i<3 ;i++) {
 		if (top[i] < current_elf) {
-			top[i] ^= current_elf;
-			current_elf ^= top[i];
-			top[i] ^= current_elf;
+			swap(&top[i], &current_elf);
 		}
 	}
 }
 
 int main() {
 	char buffer[LINE_LEN] = "";
-	FILE *fp = fopen("./input.txt", "r");
-	unsigned long current_elf = 0;
 	unsigned long top[3] = {0};
+	unsigned long current_elf = 0;
+	unsigned long current_line = 0;
+	FILE *fp = fopen("./input.txt", "r");
 
 	if (!fp) {
-		perror("file open error");
+		perror("cannot read file ./input.txt");
 		return 1;
 	}
 
 	while (fgets(buffer, LINE_LEN, fp)) {
+		// empty line -> end of one elf
 		if (buffer[0] == '\n') {
 			process_elf(top, current_elf);
 			current_elf = 0;
 			continue;
 		}
-		errno = 0; // for strtol
-		current_elf += strtoul(buffer, NULL, 10);
-		if (errno == EINVAL) {
-			perror("strtoul EINVAL");
-			fclose(fp);
-			return 1;
-		} else if (errno == ERANGE) {
-			perror("strtoul ERANGE");
-			fclose(fp);
+		
+		// shouldn't happen, but make sure that line wasn't longer than buffer
+		if (strchr(buffer, '\n') == NULL) {
+			perror("line length longer than expected");
 			return 1;
 		}
+
+		errno = 0; // for strtoul
+		current_line = strtoul(buffer, NULL, 10);
+		if (errno == EINVAL) {
+			perror("could not parse number");
+			return 1;
+		} else if (errno == ERANGE) {
+			perror("read number exceeds my capacity");
+			return 1;
+		}
+		
+		// shouldn't happen, but make sure that sum isn't larger than type limit
+		if (current_line > 0 && current_elf > (ULONG_MAX - current_line)) {
+			perror("total number of one elf exceeds my capacity");
+			return 1;
+		}
+		current_elf += current_line;
+		current_line = 0;
 	}
-	fclose(fp);
 
 	if (current_elf != 0) {
 		process_elf(top, current_elf);
 	}
+	if ((top[1] > 0 && top[2] > (ULONG_MAX - top[1])) ||
+	    ((top[1] + top[2]) > 0 && top[0] > (ULONG_MAX - (top[1]+top[2])))) {
+		perror("total of the top 3 elves exceeds my capacity");
+		return 1;
+	}
 
-	printf("%d\n%d\n", top[0], (top[0]+top[1]+top[2]));
+	printf("%ld\n%ld\n", top[0], (top[0]+top[1]+top[2]));
 	return 0;
 }
