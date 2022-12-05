8d3dd842be0db1e1ff879154beeed46b7bf9c5d3
bebna <mail@bebna.de>
2022-12-05 22:50:36 +0100

    2022-12-05T22:50:36+01:00

--- 2/rock_paper_scissors.go
+++ 2/rock_paper_scissors.go
@@ -16,17 +16,17 @@ func main() {
 	}
 	defer file.Close()
 
-	// fmt.Println(handle_input(strings.NewReader("A Y\nB X\nC Z")))
+	fmt.Println(handle_input(strings.NewReader("A Y\nB X\nC Z")))
 	fmt.Println(handle_input(file))
 }
 
 func handle_input(input io.Reader) uint {
 	sum := uint(0)
-	selection_points := map[string]uint{"X":1, "Y":2, "Z":3}
+	selection_points := map[string]uint{"X":0, "Y":3, "Z":6}
 	outcome_points := map[string]map[string]uint{
-		"A":{"X":3,"Y":6,"Z":0},
-		"B":{"X":0,"Y":3,"Z":6},
-		"C":{"X":6,"Y":0,"Z":3},
+		"A":{"X":3,"Y":1,"Z":2},
+		"B":{"X":1,"Y":2,"Z":3},
+		"C":{"X":2,"Y":3,"Z":1},
 	}
 	scanner := bufio.NewScanner(input)
 	scanner.Split(bufio.ScanLines)
